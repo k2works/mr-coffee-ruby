7d4fc3e1f5da09b8296bfd76beeebc5f8249b9ae
k2works <kakimomokuri@gmail.com>
2019-05-09 14:32:39 +0900

    chore: Sinatraアプリケーションのセットアップ

---
+++ src/spec/server_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+# Tests for server.rb
+describe 'HelloWorld Service' do
+  include Rack::Test::Methods
+
+  # Test for HTTP GET for URL-matching pattern '/'
+  it "should return successfully on GET" do
+    get '/hello-world'
+    expect(last_response).to be_ok
+    json_result = JSON.parse(last_response.body)
+    expect(json_result["Output"]).to eq("Hello World!")
+  end
+
+  # Test for HTTP POST for URL-matching pattern '/'
+  it "should return successfully on POST" do
+    post '/hello-world'
+    expect(last_response).to be_ok
+    expect(json_result["Output"]).to eq("Hello World!")
+  end
+end
