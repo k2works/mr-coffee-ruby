552189addddf8f39ef7c5e4c8de78b105dfae482
k2works <kakimomokuri@gmail.com>
2019-05-14 11:53:09 +0900

    style: フォーマット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,27 @@
 
 class App < Sinatra::Base
   configure do
-    if ENV["SINATRA_ENV"] == "production"
-      set views: "views"
-      set public_folder: "views/public"
+    if ENV['SINATRA_ENV'] == 'production'
+      set views: 'views'
+      set public_folder: 'views/public'
     else
-      set views: "app/views"
-      set public_folder: "app/views/public"
+      set views: 'app/views'
+      set public_folder: 'app/views/public'
     end
   end
 
   ##################################
   # For the index page
   ##################################
-  get "/" do
+  get '/' do
     erb :index
   end
 
-  get "/contact" do
+  get '/contact' do
     erb :contact
   end
 
-  post "/contact" do
+  post '/contact' do
     item = Contact.new(id: SecureRandom.uuid, ts: Time.now)
     item.name = params[:name]
     item.email = params[:email]
@@ -31,7 +31,7 @@ class App < Sinatra::Base
     item.message = params[:message]
     item.save!
 
-    @message = "お問い合わせを送信しました"
+    @message = 'お問い合わせを送信しました'
     erb :contact
   end
 end
--- app/controllers/contact_api_controller.rb
+++ app/controllers/contact_api_controller.rb
@@ -4,24 +4,24 @@ class App < Sinatra::Base
   ##################################
   # Return JSON
   ##################################
-  post "/api/contact/create" do
+  post '/api/contact/create' do
     content_type :json
     service = ContactService.new
     service.create
-    { Message: "問い合わせテーブルを作成しました" }.to_json
+    { Message: '問い合わせテーブルを作成しました' }.to_json
   end
 
-  post "/api/contact/save" do
+  post '/api/contact/save' do
     content_type :json
     service = ContactService.new
     service.save(params)
-    { Message: "問い合わせを送信しました" }.to_json
+    { Message: '問い合わせを送信しました' }.to_json
   end
 
-  post "/api/contact/drop" do
+  post '/api/contact/drop' do
     content_type :json
     service = ContactService.new
     service.drop
-    { Message: "問い合わせテーブルを削除しました" }.to_json
+    { Message: '問い合わせテーブルを削除しました' }.to_json
   end
 end
